7fc7e0b5be35d04c85605eba92df4014826dc7e2
Amir Mullagaliev <amirmullagaliev@gmail.com>
2021-06-07 23:33:11 +0500

    Integrate Celery; Update OrderCreateView to send an email; Add some generic views for Book;

--- store/views.py
+++ store/views.py
@@ -1,9 +1,23 @@
+from bookstore.celery import send_email
+from datetime import datetime, timedelta
+
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 from store.serializers import AuthorSerializer, BookSerializer, OrderSerializer
 from store.models import Author, Book, Order
 from store.filters import BookFilter
+
+
+class BookCreateView(generics.CreateAPIView):
+    serializer_class = BookSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class BookUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    permission_classes = (permissions.IsAdminUser,)
 
 
 class BookListView(generics.ListAPIView):
@@ -25,3 +39,11 @@ class OrderCreateView(generics.CreateAPIView):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        send_date = datetime.utcnow() + timedelta(seconds=10)
+        email = self.request.user.email
+        message = "lol"
+
+        send_email.add.apply_async((email, message), eta=send_date)
+        return super().post(request, *args, **kwargs)
